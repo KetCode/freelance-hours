377a1b80c53814359ecf83aaf3b4b8cb09a79a29
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-20 11:51:00 -0300

    fix: welcome view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,6 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return WelcomeController::class;
+        return view('welcome');
     }
 }
