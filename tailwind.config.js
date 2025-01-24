de87b3a6d08b37ebdd2d42baafd6e0eee1f01372
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-24 12:43:42 -0300

    feat: remove unused content and add roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
         './resources/**/*.blade.php',
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Segoe UI', 'sans-serif'],
+                sans: ['Segoe UI', 'Roboto', 'sans-serif'],
             },
         },
     },
