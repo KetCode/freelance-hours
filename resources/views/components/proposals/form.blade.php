f2d36c9d285909e0d2625adbe031b3bc6de5b883
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-26 00:28:58 -0300

    feat: modal title next to close button

--- resources/views/components/proposals/form.blade.php
+++ resources/views/components/proposals/form.blade.php
@@ -1,9 +1,7 @@
-<x-ui.modal>
+<x-ui.modal title="Envie sua proposta">
     <form class="flex flex-col gap-6" wire:submit="save">
-        <div>
-            <div class="text-[28px]">Envie sua proposta</div>
-            <div class="text-[16px] text-[#C3C3D1]">Faça sua oferta em horas mensais que você pode contribuir com o projeto.</div>
-        </div>
+        <div class="text-[16px] text-[#C3C3D1]">Faça sua oferta em horas mensais que você pode contribuir com o projeto.</div>
+
         <div class="flex gap-4">
             <div class="w-2/3 gap-2 flex flex-col">
                 <label class="text-[14px] text-[#C3C3D1]">E-mail</label>
