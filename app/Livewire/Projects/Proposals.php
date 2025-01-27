9615b7f53fbd11249121a6975f545dd17cd2ac66
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-27 11:06:25 -0300

    feat: ordering proposals by hours, change last published proposal time and add load more function

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,31 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
 
     public Project $project;
+    public int $quantity = 5;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()->orderBy('hours')->paginate($this->quantity);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->quantity += 5;
+    }
 
     public function render()
     {
