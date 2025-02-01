62f51bba31a675429a8350576b26847b34cbab44
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-01 11:34:54 -0300

    feat: about content page

---
+++ resources/views/livewire/projects/about.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-[#10101E] divide-[#1E1E2C] divide-y p-[40px] rounded-[10px] border-[#1E1E2C] border w-3/5">
+    <div class="text-[28px] text-white leading-9 pb-4 border-none">Sobre o Freelance Hours</div>
+    <div class="text-[20px] text-white leading-7 pb-4 border-none">O que é o Freelance Hours?</div>
+    <div class="text-[16px] text-white leading-6 pb-4 border-none">O Freelance Hours é uma plataforma online que conecta <strong>empresas ou indivíduos</strong> com projetos a <strong>desenvolvedores qualificados</strong> que oferecem <strong>serviços personalizados</strong>. Nosso objetivo é facilitar o processo de contratação para ambos os lados, permitindo que desenvolvedores e clientes se conectem de forma prática e eficiente.</div>
+    <div class="text-[20px] text-white leading-7 border-none">Como funciona?</div>
+    <div class="flex flex-col border-none py-2">
+        <div class="flex items-baseline gap-2 px-5 py-2">
+            <div class="w-[10px] h-[10px] border rounded shrink-0"></div>
+            <p><strong class="text-white">Cadastro de Projetos</strong>: Se você tem um projeto, basta criar uma descrição detalhada no nosso site. Isso ajuda a atrair desenvolvedores que possuam as habilidades necessárias para o que você precisa.</p>
+        </div>
+        <div class="flex items-baseline gap-2 px-5 py-2">
+            <div class="w-[10px] h-[10px] border rounded shrink-0"></div>
+            <p><strong class="text-white">Propostas de Desenvolvedores</strong>: Desenvolvedores cadastrados no site podem visualizar o projeto e, caso estejam interessados, enviar propostas com valores por hora.</p>
+        </div>
+        <div class="flex items-baseline gap-2 px-5 py-2">
+            <div class="w-[10px] h-[10px] border rounded shrink-0"></div>
+            <p><strong class="text-white">Seleção e Negociação</strong>: Após receber as propostas, você pode analisar o perfil dos desenvolvedores, verificar as habilidades e experiência deles, e escolher aquele que melhor se adequa ao seu projeto.</p>
+        </div>
+        <div class="flex items-baseline gap-2 px-5 py-2">
+            <div class="w-[10px] h-[10px] border rounded shrink-0"></div>
+            <p><strong class="text-white">Execução do Projeto</strong>: Com a proposta aceita, o projeto começa a ser executado! O desenvolvedor trabalha nas horas acordadas, e você tem a flexibilidade de acompanhar o progresso..</p>
+        </div>
+        <div class="flex items-baseline gap-2 px-5 py-2">
+            <div class="w-[10px] h-[10px] border rounded shrink-0"></div>
+            <p><strong class="text-white">Pagamento por Hora</strong>: O pagamento é feito conforme o número de horas trabalhadas.</p>
+        </div>
+    </div>
+</div>
